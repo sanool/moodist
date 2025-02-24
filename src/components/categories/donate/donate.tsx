bce2249c7de1d699c4af0f760aafe1fb2052e90a
sanool <sanool@gmail.com>
2025-02-24 13:29:25 +0000

    updated

--- src/components/categories/donate/donate.tsx
+++ src/components/categories/donate/donate.tsx
@@ -6,22 +6,6 @@ import styles from './donate.module.css';
 
 export function Donate() {
   return (
-    <div className={styles.donate}>
-      <div className={styles.iconContainer}>
-        <div className={styles.tail} />
-        <div aria-hidden="true" className={styles.icon}>
-          <FaCoffee />
-        </div>
-      </div>
-
-      <div className={styles.title}>Support Me</div>
-      <p className={styles.desc}>Help me keep Moodist ad-free.</p>
-      <SpecialButton
-        className={styles.button}
-        href="https://buymeacoffee.com/remvze"
-      >
-        Donate Today
-      </SpecialButton>
-    </div>
+    <div></div>
   );
 }
